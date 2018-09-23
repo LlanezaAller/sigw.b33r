90239eb4ba424a888efbf49cc6d86f1bd4d60c14
María Hernández <UO229803@uniovi.es>
2018-09-23 11:38:41 +0200

    Clean markers

--- B33rUI/public/javascripts/script.js
+++ B33rUI/public/javascripts/script.js
@@ -1,41 +1,44 @@
 var map;
+var actualMarker;
+var barMarkers = [];
+var taxiMarkers = [];
 
 function initMap() {
-    var oviedo_eii = new google.maps.LatLng(43.354810,-5.851805);
+    var oviedo_eii = new google.maps.LatLng(43.354810, -5.851805);
     var misOpciones = {
-      center: oviedo_eii,
-      zoom: 18,
-      mapTypeId: 'satellite',
-      tilt: 45,
-      heading: 90,
-      streetViewControl: false,
-      mapTypeControl: false,
-      rotateControl:true,
-      fullscreenControl:false
+        center: oviedo_eii,
+        zoom: 18,
+        mapTypeId: 'satellite',
+        tilt: 45,
+        heading: 90,
+        streetViewControl: false,
+        mapTypeControl: false,
+        rotateControl: true,
+        fullscreenControl: false
     };
-    
-  map = new google.maps.Map(document.getElementById("map"), misOpciones);
+
+    map = new google.maps.Map(document.getElementById("map"), misOpciones);
     map.addListener('click', function(event) {
         addMarker(event.latLng);
     });
-    
+
 }
 
-function readPoints(json){
-    var jsonObj=JSON.parse(json);
+function readPoints(json) {
+    var jsonObj = JSON.parse(json);
     for (let i in json) {
-        var lat=0;
-        var lon=0;
+        var lat = 0;
+        var lon = 0;
         for (let j in json[i]) {
-          if (json[i][j].hasOwnProperty('latitude')) {
-            lat=json[i][j];
-          }else if (json[i][j].hasOwnProperty('longitude')) {
-            lon=json[i][j];
-          }
-          if (lat!=0 && lon!=0)
-            addMarker(new google.maps.LatLng(lat,lon));
+            if (json[i][j].hasOwnProperty('latitude')) {
+                lat = json[i][j];
+            } else if (json[i][j].hasOwnProperty('longitude')) {
+                lon = json[i][j];
+            }
+            if (lat != 0 && lon != 0)
+                addMarker(new google.maps.LatLng(lat, lon));
         }
-      }
+    }
 }
 
 function addMarker(location) {
@@ -43,12 +46,30 @@ function addMarker(location) {
         position: location,
         map: map
     });
-    if(actualMarker !== undefined)
-        actualMarker.setMap(null);
-    actualMarker=marker;
+    if (this.actualMarker !== undefined)
+        this.actualMarker.setMap(null);
+    this.actualMarker = marker;
 }
 
+function cleanBarMarkers() {
+    for (var i = 0; i < this.barMarkers.length; i++) {
+        this.barMarkers[i].setMap(null);
+    }
+    this.barMarkers = [];
+}
 
-$(document).ready(function(){
+function cleanTaxiMarkers() {
+    for (var i = 0; i < this.taxiMarkers.length; i++) {
+        this.taxiMarkers[i].setMap(null);
+    }
+    this.taxiMarkers = [];
+}
+
+function cleanMap() {
+    cleanBarMarkers();
+    cleanTaxiMarkers();
+}
+
+$(document).ready(function() {
     $('#slider').slider();
 });
